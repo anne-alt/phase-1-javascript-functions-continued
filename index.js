1d0dd75c9651ea0ffb0ca66fb60a75ce1a0994a7
Anne Ogongo <anneogongo98@gmail.com>
2022-08-17 17:58:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+};
+ function mondayWork(stuff = "go to the office") {
+    return (`This Monday, I will ${stuff}.`);
+ };
+ function wrapAdjective(word = "*") {
+    return function(say = "special") {
+        return  (`You are ${word}${say}${word}!`);
+    };
+ };
